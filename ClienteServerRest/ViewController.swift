cdc7c01cdb405ac588c591f3c58d021d3803a817
Carmelo Vega <carmelo@iMac-de-admin.local>
2019-01-31 10:39:18 +0100

    Initial Commit

---
+++ ClienteServerRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClienteServerRest
+//
+//  Created by Carmelo Vega on 31/01/2019.
+//  Copyright © 2019 Carmelo Vega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
